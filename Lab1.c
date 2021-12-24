aa5aeb64ca53d9fea24c6274002bbb5c51b93e70
anna <borzdunanna@gmail.com>
2021-12-25 04:47:25 +0700

    gitv2.2

--- Lab1.c
+++ Lab1.c
@@ -3,12 +3,12 @@
 #include <math.h>
 int main()
 {
-   // float a;
+    float a;
    float a, c;
     char b[200];
     scanf("%f", &a);
     scanf("%d", &b); //scanf("%s", &b);
 
     printf("%f\n", a);
-    //printf("%s", b);
+    printf("%s", b);
 }
